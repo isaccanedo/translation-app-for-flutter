89cd5338f054edcff6eff4d0616cc74c48ac9a21
isaccanedo <isaccanedo@gmail.com>
2021-11-16 14:13:13 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'speech_screen.dart';
+
+void main() {
+  runApp(MyApp()); //Runs MyApp
+}
+
+class MyApp extends StatelessWidget {
+  //Extends a StatelessWidget
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google Ceviri', //Title
+      debugShowCheckedModeBanner: false, //Removes debug mode banner
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ), //Change
+      home: SpeechScreen(), //Opens SpeechScreen
+    );
+  }
+}
